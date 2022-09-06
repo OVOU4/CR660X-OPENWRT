735ee7f04dc9a76fc96cccdfc3fb95c22683fbfc
OVO <65055474+OVOU4@users.noreply.github.com>
2022-09-06 13:46:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,4 +21,4 @@ git clone https://github.com/Zxilly/UA2F.git package/UA2F
 
 sed -i 's/5.10/5.15/g' target/linux/ramips/Makefile
 sed -i 's/dnsmasq/dnsmasq-full/g' include/target.mk
-sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
